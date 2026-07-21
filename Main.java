885575e797b5b103bfe9858aafa3ff4506eba402
Uziel Fernandes <uzielfern07@gmail.com>
2026-07-22 00:23:26 +0530

    Added Anatomy of Java

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main  { //classs!!!!: Here class also contains the access modifier and name
+    public static void main(String[] args) {  //function which contains the method and the access modifier
+        System.out.println("HELLO!!!!");   //System, member and the work
+    }
+}
