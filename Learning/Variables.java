0cc4eca135aded7ba205f095a6b182c4fcef6ec9
Uziel Fernandes <uzielfern07@gmail.com>
2026-07-22 19:39:20 +0530

    added input

---
+++ Learning/Variables.java
@@ -0,0 +1,11 @@
+public class Variables  { 
+    public static void main(String[] args) {  
+        System.out.println("HELLO!!!!");   
+        int age = 5;
+        String text = "hello everyone!";
+
+        age = 6;
+        System.out.println("my age " + age);
+        System.out.println("I SAY " + text);
+    }
+}
